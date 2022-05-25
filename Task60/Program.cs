90bb655e0c4bdf24e67d59c39241032707155278
Ekaterina Rudneva <103060211+rudninka@users.noreply.github.com>
2022-05-25 20:09:16 +0300

    Update Program.cs

--- Task60/Program.cs
+++ Task60/Program.cs
@@ -30,5 +30,19 @@ void Print(int[, ,] array)
     }
 }
 int[, ,] mass = new int[3, 4, 3];
+int min = 10;
+if (mass.Length < 99 - 10)
+{
+for (int x = 0; x < mass.GetLength(0); x++)
+{
+    for (int y = 0; y < mass.GetLength(1); y++)
+    {
+        for (int z = 0; z < mass.GetLength(2); z++)
+        {
+            mass[x, y, z] = min;
+            min++;
+        }
+    }
+}}
 FillArray(mass);
 Print(mass);
