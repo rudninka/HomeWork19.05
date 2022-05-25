e8208a89ad4d0ee8cb1f6830e2aae7bccdd45afd
Ekaterina <ekaterina_r21@mail.ru>
2022-05-25 14:08:33 +0300

    Трехмерный массив

---
+++ Task60/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся
+//двузначных чисел. Напишите программу, которая будет построчно выводить
+//массив, добавляя индексы каждого элемента.
+void FillArray(int[, ,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+}
+void Print(int[, ,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k] + " " + (i, j, k) + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+int[, ,] mass = new int[3, 4, 3];
+FillArray(mass);
+Print(mass);
